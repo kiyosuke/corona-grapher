b01f258fb01a65e2d620b11e0c5eeb3583055b21
kiyosuke <k.kiyosuke1999@gmail.com>
2020-03-26 23:33:20 +0900

    [remove] API確認用の処理を削除

--- app/src/main/java/com/kiyosuke/corona_grapher/di/module.kt
+++ app/src/main/java/com/kiyosuke/corona_grapher/di/module.kt
@@ -3,12 +3,14 @@ package com.kiyosuke.corona_grapher.di
 import com.kiyosuke.corona_grapher.ui.MainViewModel
 import com.kiyosuke.corona_grapher.ui.locations.detail.LocationDetailViewModel
 import com.kiyosuke.corona_grapher.ui.locations.list.LocationListViewModel
+import com.kiyosuke.corona_grapher.ui.map.MapViewModel
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val androidModule = module {
 
-    viewModel { MainViewModel(get()) }
+    viewModel { MainViewModel() }
+    viewModel { MapViewModel(get()) }
     viewModel { LocationDetailViewModel(get()) }
     viewModel { LocationListViewModel(get()) }
 }
--- app/src/main/java/com/kiyosuke/corona_grapher/ui/MainActivity.kt
+++ app/src/main/java/com/kiyosuke/corona_grapher/ui/MainActivity.kt
@@ -3,35 +3,14 @@ package com.kiyosuke.corona_grapher.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.kiyosuke.corona_grapher.R
-import com.kiyosuke.corona_grapher.databinding.ActivityMainBinding
-import com.kiyosuke.corona_grapher.model.Location
-import com.kiyosuke.corona_grapher.util.ext.dataBinding
-import com.kiyosuke.corona_grapher.util.ext.observeNonNull
-import org.koin.android.viewmodel.ext.android.viewModel
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
-    private val viewModel: MainViewModel by viewModel()
-    private val binding: ActivityMainBinding by dataBinding()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        viewModel.locations.observeNonNull(this) { locations ->
-
-        }
-
-        viewModel.locationLoadState.observeNonNull(this) { state ->
-            Timber.d("state=$state")
-        }
-
         Timber.d("onCreate")
-
-        viewModel.refresh()
     }
 
-    private fun requestLocationDetail(location: Location) {
-        viewModel.requestDetail(location.id)
-    }
 }
