b3757af14dba427c46084c681f96a9ce71da4c4c
kiyosuke <k.kiyosuke1999@gmail.com>
2020-03-28 07:11:53 +0900

    [update] 世界合計感染者数をメモリキャッシュする処理を実装

--- data/repository/src/main/java/com/kiyosuke/corona_grapher/data/repository/impl/CoronavirusDataRepository.kt
+++ data/repository/src/main/java/com/kiyosuke/corona_grapher/data/repository/impl/CoronavirusDataRepository.kt
@@ -12,16 +12,19 @@ import com.kiyosuke.corona_grapher.model.Location
 import com.kiyosuke.corona_grapher.model.LocationId
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import timber.log.Timber
 
 internal class CoronavirusDataRepository(
     private val api: CoronavirusTrackerApi,
     private val database: CoronavirusDatabase,
-    private val cache: MemoryCache<LocationId, Location.Detail>
+    private val cache: MemoryCache<LocationId, Location.Detail>,
+    private val latestCache: MemoryCache<String, Latest>
 ) : CoronavirusRepository {
 
     override suspend fun refreshLocations() {
         val locations = api.allLocations()
         database.saveLocations(locations)
+        latestCache.put(KEY_LATEST, locations.latest.toLatest())
     }
 
     override fun locations(): Flow<List<Location>> {
@@ -30,9 +33,9 @@ internal class CoronavirusDataRepository(
 
     override suspend fun location(id: LocationId): Location.Detail {
         // メモリキャッシュが有効なら利用
-        val memCache = cache.get(id)?.value
+        val memCache = cache.get(id)
         if (memCache != null) {
-            return memCache
+            return memCache.value
         }
         val data = api.location(id.id).location.toLocation()
         cache.put(id, data)
@@ -40,6 +43,17 @@ internal class CoronavirusDataRepository(
     }
 
     override suspend fun latest(): Latest {
-        return api.latest().latest.toLatest()
+        val memCache = latestCache.get(KEY_LATEST)
+        if (memCache != null) {
+            Timber.d("latest use cache")
+            return memCache.value
+        }
+        val latest = api.latest().latest.toLatest()
+        latestCache.put(KEY_LATEST, latest)
+        return latest
+    }
+
+    companion object {
+        private const val KEY_LATEST = "key_latest"
     }
 }
--- data/repository/src/main/java/com/kiyosuke/corona_grapher/data/repository/module.kt
+++ data/repository/src/main/java/com/kiyosuke/corona_grapher/data/repository/module.kt
@@ -3,16 +3,21 @@ package com.kiyosuke.corona_grapher.data.repository
 import com.kiyosuke.corona_grapher.data.memory.Expiration
 import com.kiyosuke.corona_grapher.data.memory.MemoryCache
 import com.kiyosuke.corona_grapher.data.repository.impl.CoronavirusDataRepository
+import com.kiyosuke.corona_grapher.model.Latest
 import com.kiyosuke.corona_grapher.model.Location
 import com.kiyosuke.corona_grapher.model.LocationId
 import org.koin.dsl.module
 
 val repoModule = module {
-    factory {
-        MemoryCache.builder<LocationId, Location.Detail>()
+    single<CoronavirusRepository> {
+        val locationCache = MemoryCache.builder<LocationId, Location.Detail>()
             .setExpireTime(Expiration.bySeconds(300))
             .setMaxSize(20)
             .build()
+        val latestCache = MemoryCache.builder<String, Latest>()
+            .setExpireTime(Expiration.bySeconds(300))
+            .setMaxSize(1)
+            .build()
+        CoronavirusDataRepository(get(), get(), locationCache, latestCache)
     }
-    single<CoronavirusRepository> { CoronavirusDataRepository(get(), get(), get()) }
 }
