ba04778cea06d0de05047d6eeb9a27ea8989d562
kiyosuke <k.kiyosuke1999@gmail.com>
2020-03-29 00:02:27 +0900

    [fix] AndroidThreeTenの初期化処理を実装

--- app/src/main/java/com/kiyosuke/corona_grapher/App.kt
+++ app/src/main/java/com/kiyosuke/corona_grapher/App.kt
@@ -1,6 +1,7 @@
 package com.kiyosuke.corona_grapher
 
 import android.app.Application
+import com.jakewharton.threetenabp.AndroidThreeTen
 import com.kiyosuke.corona_grapher.data.api.apiModule
 import com.kiyosuke.corona_grapher.data.db.dbModule
 import com.kiyosuke.corona_grapher.data.repository.repoModule
@@ -13,6 +14,8 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        AndroidThreeTen.init(this)
 
         Timber.plant(Timber.DebugTree())
 
